99325a2a5143a55c085d090b1c9a29518e119332
ael-kass <aelkassimi321@gmail.com>
2023-01-17 14:10:35 +0100

    remove the line for creating phpinfo file

--- nginx/tools/setup.sh
+++ nginx/tools/setup.sh
@@ -1,7 +1,6 @@
 cp ./conf/default.conf /etc/nginx/conf.d/default.conf
 
 
-echo "<?php phpinfo(); ?>" >> /usr/share/nginx/html/info.php
 chmod 777 /etc/nginx/nginx.conf
 echo "daemon off;" >> /etc/nginx/nginx.conf
 service nginx start 
